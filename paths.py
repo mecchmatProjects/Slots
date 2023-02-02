470862e006c4f321f5b2e3f752e164072d7d937c
mecchmatProjects <33247296+mecchmatProjects@users.noreply.github.com>
2023-02-02 15:54:06 +0200

    update script

--- paths.py
+++ paths.py
@@ -1,64 +1,22 @@
-"""
-n, k, a, b, d.
-"""
-"""
-n, k, a, b, d = map(int,input().split())
+#!usr/bin/python env 
 
-graph = [ [] for _ in range(n) ]
+from collections import Counter
 
-for _ in range(k):
-	x,y = map(int,input().split())
-	graph[x-1].append(y-1)
-"""
-count = 0
+degrees = Counter({})
 
-degs = []
-
-#print(graph)
-
-def dfs(graph,a,b,d,path):
-	global count, deg
-	print(a)
-	path.append(a)	
-	if d==0:
-		count += 1
-		vals = []
-		for day in path:
-			for el in graph[day]:
-				if el in path:
-					continue
-				if el in vals:
-					continue
-				vals.append(el) 
-		degs.append(vals)
-		return
-	
-	
-	print("path:",path)
-	print(a,graph)	
-	for next in graph[a]:
-		if next in path:
-			continue
-		dfs(graph,next,b,d-1,path)
-
-#dfs(graph,a-1,b-1,2,[])
-#print(count, degs)
-
-
-degrees = []
-
-
+WIDTH = 6
+HEIGHT = 3
 
 def dfs2(W,H, x0,y0,d,path=[]):
 
 	global degrees
 	steps = ((0,1), (1,1), (1,0), (1,-1), (0,-1),(-1,-1),(-1,0),(-1,1))
 	
-	print("xy",x0,y0)
+	# print("xy",x0,y0)
 	v = W*y0 + x0
 	path.append(v) 
 	if len(path)==d: 
-		print("path f:",path)
+		# print("path f:",path)
 		vals = []
 		for cell in path:
 			for nb in steps:
@@ -73,17 +31,18 @@ def dfs2(W,H, x0,y0,d,path=[]):
 				if val in vals:
 					continue
 				vals.append(val)
-		degrees.append(len(vals))
-		print("v:",vals)
+		# degrees.append(len(vals))
+		degrees.update([len(vals)])
+		# print("v:",vals)
 		return
-	print("s=",x0,y0,v, path)
+	# print("s=",x0,y0,v, path)
 	for nb in steps:
 		x1 = x0 + nb[0]
 		y1 = y0 + nb[1]
 		if x1<0 or x1>=W or y1<0 or y1>=H:
 			continue
 		v1 = W*y1 + x1
-		print("v1",v1)
+		# print("v1",v1)
 		if v1 in path:
 			continue
 		dfs2(W,H,x1,y1,d,path)
@@ -92,3 +51,22 @@ def dfs2(W,H, x0,y0,d,path=[]):
 
 dfs2(6,3,0,0,2,[])
 print(degrees)
+
+degrees.clear()
+
+FNAME = "Coefs.txt"
+f = open(FNAME,"w")
+
+
+for i in range(WIDTH):
+        for j in range(HEIGHT):
+                dfs2(WIDTH,HEIGHT,i,j,4,[])
+                # delete C^{d-1}_{d}?
+                print(degrees, file=f)
+                degrees.clear() # = Counter({})
+                
+                
+f.close()
+
+
+
